fb8eb4c32ffead6e614ea7a304498cd2a22e5b6b
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-28 00:20:39 +0300

    feat: combobox

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,15 +1,38 @@
 import type Card from '../types/Card';
 import cardsData from '../data/cards.json';
+import { Combobox } from '@headlessui/react';
+import { useState } from 'react';
 
 export default function Game() {
     const cards: Card[] = cardsData;
+    const cardNames: string[] = cards.map((card) => card.name);
     const card = cards[Math.floor(Math.random() * cards.length)];
 
-    console.log(card);
+    const [selectedCard, setSelectedCard] = useState('');
+    const [query, setQuery] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
+
+    // console.log(card);
+
+    const filteredCards = cardNames.filter((card) => card.toLowerCase().includes(query.toLowerCase()));
 
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div>
+            <input value={selectedCard ?? query} onChange={(e) => {
+                setQuery(e.target.value);
+                setSelectedCard('');
+            }}/>
+
+            {query && filteredCards.length > 0 && (
+                <ul>
+                    {filteredCards.map((card) => (
+                        <li key={card} onClick={() => {
+                            setSelectedCard(card);
+                            setQuery("");
+                        }}>{card}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
     )
 }
