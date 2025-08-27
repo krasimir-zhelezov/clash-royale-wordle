1afbabd42d81ce5155da6394730223c9a512daf9
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-27 23:46:40 +0300

    feat: card loader

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,4 +1,11 @@
+import type Card from '../types/Card';
+import cardsData from '../data/cards.json';
+
 export default function Game() {
+    const cards: Card[] = cardsData;
+
+    console.log(cards);
+
     return (
         <h1>Game</h1>
     )
