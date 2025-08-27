f11ee3d7df9d4d5835f7f5abf8e5b69a9e0d72ce
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-28 00:37:29 +0300

    fix: on click for combobox

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -8,39 +8,32 @@ export default function Game() {
     const card = cards[Math.floor(Math.random() * cards.length)];
 
     const [cardNames, setCardNames] = useState(cards.map(card => card.name));
-    const [selectedCard, setSelectedCard] = useState('');
     const [query, setQuery] = useState('');
 
     const filteredCards = cardNames.filter((card) => card.toLowerCase().includes(query.toLowerCase()));
 
-    function guessCard() {
-        const cardName = filteredCards[0];
+    function guessCard(chosenCard?: string ) {
+        const cardName = chosenCard || filteredCards[0];
+        
+        console.log(cardName);
 
         setQuery('');
 
         if (!cardName) return;
-
-        console.log(query);
-        console.log(cardName);
         
         setCardNames(cardNames.filter(card => card !== cardName));
-        console.log(cardNames);
     }
 
     return (
         <div>
             <input value={query} onChange={(e) => {
                 setQuery(e.target.value);
-                setSelectedCard('');
             }} onKeyDown={(e) => e.key === 'Enter' && guessCard()}/>
 
             {query && filteredCards.length > 0 && (
                 <ul>
                     {filteredCards.map((card) => (
-                        <li key={card} onClick={() => {
-                            setSelectedCard(card);
-                            setQuery("");
-                        }}>{card}</li>
+                        <li key={card} onClick={() => guessCard(card)}>{card}</li>
                     ))}
                 </ul>
             )}
