7a84d062adf3a3c2aafbf494b1305c522cc0dcbf
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-28 00:28:55 +0300

    feat: combobox limited user input

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -5,23 +5,34 @@ import { useState } from 'react';
 
 export default function Game() {
     const cards: Card[] = cardsData;
-    const cardNames: string[] = cards.map((card) => card.name);
     const card = cards[Math.floor(Math.random() * cards.length)];
 
+    const [cardNames, setCardNames] = useState(cards.map(card => card.name));
     const [selectedCard, setSelectedCard] = useState('');
     const [query, setQuery] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
-
-    // console.log(card);
 
     const filteredCards = cardNames.filter((card) => card.toLowerCase().includes(query.toLowerCase()));
 
+    function guessCard() {
+        const cardName = filteredCards[0];
+
+        setQuery('');
+
+        if (!cardName) return;
+
+        console.log(query);
+        console.log(cardName);
+        
+        setCardNames(cardNames.filter(card => card !== cardName));
+        console.log(cardNames);
+    }
+
     return (
         <div>
-            <input value={selectedCard ?? query} onChange={(e) => {
+            <input value={query} onChange={(e) => {
                 setQuery(e.target.value);
                 setSelectedCard('');
-            }}/>
+            }} onKeyDown={(e) => e.key === 'Enter' && guessCard()}/>
 
             {query && filteredCards.length > 0 && (
                 <ul>
