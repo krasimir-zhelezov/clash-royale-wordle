947915e2239106ffde4fdaf70756aadbe3ab82b4
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-28 21:34:59 +0300

    fix: table cell width and height

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -66,12 +66,12 @@ export default function Game() {
                     <tbody className="border-black border-1 text-white">
                         {guessedCards.slice().reverse().map((card) => (
                             <tr key={card.name}>
-                                <td className="w-40 h-40"><img src={card.image}/></td>
-                                <td className={`w-40 h-40 border-black border-1 rounded-md ${randomCard.rarity === card!.rarity ? 'bg-green-700' : 'bg-red-700'}`}>{card.rarity}</td>
-                                <td className={`w-40 h-40 border-black border-1 rounded-md ${randomCard.type === card!.type ? 'bg-green-700' : 'bg-red-700'}`}>{card.type}</td>
-                                <td className={`w-40 h-40 border-black border-1 rounded-md ${randomCard.arena === card!.arena ? 'bg-green-700' : 'bg-red-700'}`}>{card.arena}</td>
-                                <td className={`w-40 h-40 border-black border-1 rounded-md ${randomCard.elixir === card!.elixir ? 'bg-green-700' : 'bg-red-700'}`}>{card.elixir}</td>
-                                <td className={`w-40 h-40 border-black border-1 rounded-md ${randomCard.releaseYear === card!.releaseYear ? 'bg-green-700' : 'bg-red-700'}`}>{card.releaseYear}</td>
+                                <td className="w-20 h-20"><img src={card.image}/></td>
+                                <td className={`w-20 h-20 border-black border-1 rounded-md ${randomCard.rarity === card!.rarity ? 'bg-green-700' : 'bg-red-700'}`}>{card.rarity}</td>
+                                <td className={`w-20 h-20 border-black border-1 rounded-md ${randomCard.type === card!.type ? 'bg-green-700' : 'bg-red-700'}`}>{card.type}</td>
+                                <td className={`w-20 h-20 border-black border-1 rounded-md ${randomCard.arena === card!.arena ? 'bg-green-700' : 'bg-red-700'}`}>{card.arena}</td>
+                                <td className={`w-20 h-20 border-black border-1 rounded-md ${randomCard.elixir === card!.elixir ? 'bg-green-700' : 'bg-red-700'}`}>{card.elixir}</td>
+                                <td className={`w-20 h-20 border-black border-1 rounded-md ${randomCard.releaseYear === card!.releaseYear ? 'bg-green-700' : 'bg-red-700'}`}>{card.releaseYear}</td>
                             </tr>
                         ))}
                     </tbody>
