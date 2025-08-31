8a3ee52d537d9159a688fe7c42fa03db5003e9c8
krasimir-zhelezov <krasizh07@gmail.com>
2025-08-31 18:09:10 +0300

    feat: title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Game from './pages/Game'
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Clash Royale Wordle";
+  }, []);
 
   return (
     <HashRouter>
