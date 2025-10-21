9fb5bc20536474b11d2ac76c7ade2029ba317b45
krasimir-zhelezov <krasizh07@gmail.com>
2025-10-21 17:33:01 +0300

    fix: ad sense link

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import './App.css'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Game from './pages/Game'
-import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    document.title = "Clash Royale Wordle";
-  }, []);
-
   return (
     <HashRouter>
       <Routes>
